7e5b9b05f1c36e9dca49bba542fe265748029387
Tonaxis <83670267+Tonaxis@users.noreply.github.com>
2024-09-01 00:56:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-console.log("Hello world!");
-
-document.querySelector("div").style.backgroundColor = "blue";
+const style = document.createElement("style");
+style.textContent = `
+div {
+  background-color: #000;
+  color: #fff;
+  border: 1px solid #f00;
+}
+`;
+document.head.appendChild(style);
