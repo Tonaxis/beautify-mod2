92387a666e35b448f6e19ab34e4baac1d62906cb
Tonaxis <83670267+Tonaxis@users.noreply.github.com>
2024-09-07 02:36:09 +0200

    update

--- dist/layer.js
+++ dist/layer.js
@@ -42,9 +42,4 @@ const extractors = require("./extractors");
 
 const results = extract(extractors);
 
-results.divs.forEach((div) => {
-  div.style.background =
-    "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)";
-});
-
 },{"./extractors":3,"beautify-dev":2}]},{},[4]);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,3 @@ const { extract } = require("beautify-dev");
 const extractors = require("./extractors");
 
 const results = extract(extractors);
-
-results.divs.forEach((div) => {
-  div.style.background =
-    "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)";
-});
